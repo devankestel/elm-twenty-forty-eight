9c1cea96a987d6eec15200d4d52e8ca20bbfc2b7
Devan Kestel <devan.kestel@gmail.com>
2017-10-08 19:58:54 -0400

    Added blocks to the model. Put an old elm app in as a guide to use while developing.

--- src/Main.elm
+++ src/Main.elm
@@ -1,19 +1,42 @@
 module Main exposing (..)
 
 import Html exposing (Html, text, div, img)
-import Html.Attributes exposing (src)
+import Html.Attributes exposing (..)
 
 
 ---- MODEL ----
 
 
+type alias Block =
+    { color : String
+    , label : String
+    , value : Int
+    }
+
+
 type alias Model =
-    {}
+    { blocks : List Block }
+
+
+blocks : List Block
+blocks =
+    [ { color = "red", label = "A", value = 2 }
+    , { color = "orange", label = "B", value = 4 }
+    , { color = "yellow", label = "C", value = 8 }
+    , { color = "green", label = "D", value = 16 }
+    , { color = "blue", label = "E", value = 32 }
+    , { color = "purple", label = "F", value = 64 }
+    , { color = "pink", label = "G", value = 128 }
+    , { color = "brown", label = "H", value = 2 }
+    , { color = "black", label = "I", value = 2 }
+    , { color = "gray", label = "J", value = 2 }
+    , { color = "gold", label = "K", value = 2 }
+    ]
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { blocks = blocks }, Cmd.none )
 
 
 
@@ -37,7 +60,22 @@ view : Model -> Html Msg
 view model =
     div []
         [ img [ src "/logo.svg" ] []
-        , div [] [ text "Your Elm App is working!" ]
+        , div [] [ text (toString model.blocks) ]
+        , blockList model.blocks
+        ]
+
+
+blockList : List Block -> Html Msg
+blockList blocks =
+    blocks
+        |> List.map block
+        |> div []
+
+
+block : Block -> Html Msg
+block block =
+    div []
+        [ div [ class "block", class block.color ] [ text block.label ]
         ]
 
 
